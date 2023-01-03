889018076877769bc925696de2b4dc2e51f51559
Jerry1811 <jerryhukpati@gmail.com>
2023-01-03 07:43:19 +0000

    revert changes

--- cypress/e2e/setup.cy.ts
+++ cypress/e2e/setup.cy.ts
@@ -4,13 +4,6 @@ const { email, password } = login_valid;
 
 describe("empty spec", () => {
   it("passes", () => {
-    // cy.login(email, password);
-    cy.visit("/");
-    cy.clock();
-    cy.get("#timerAlertButton").click();
-    cy.tick(5000);
-    cy.on("window:alert", (text) => {
-      expect(text).to.equal("Godsway says I should re run with a wrong text");
-    });
+    cy.login(email, password);
   });
 });
